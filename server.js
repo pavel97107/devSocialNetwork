fd98613dd49b8ba885eb8ae8ae5ab9e3644e3be4
pavel <pavel97107@gmail.com>
2020-09-21 15:29:19 +0300

    definingUseRouter

--- server.js
+++ server.js
@@ -4,11 +4,17 @@ const connectDB = require("./config/db");
 const app = express();
 
 //Connect Database
-connectDB()
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("API running");
 });
+
+//Define routes
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
 
 const PORT = process.env.PORT || 5000;
 
