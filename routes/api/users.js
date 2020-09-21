582da570477ee39413cd7280759599dd88c2987c
pavel <pavel97107@gmail.com>
2020-09-21 16:07:03 +0300

    addValidationInputDataUser

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,11 +1,27 @@
 const { Router } = require("express");
 const router = Router();
+const { body, validationResult } = require("express-validator");
 
 //@route GET api/users
-//@desc Test route
+//@desc Register user
 //@access Public
-router.get("/", (req, res) => {
-  res.send("User route");
-});
+router.post(
+  "/",
+  [
+    body("name", "Name is required").not().isEmpty(),
+    body("email", "Please include a valid email").not().isEmpty(),
+    body(
+      "password",
+      "Please enter a password with 6 or more characters"
+    ).isLength({ min: 6 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400).json({ errors: errors.array() });
+    }
+    res.send("User route");
+  }
+);
 
 module.exports = router;
