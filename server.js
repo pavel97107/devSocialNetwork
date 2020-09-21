c70d37d4ac92b8fc248fb61522d8deacafaba881
pavel <pavel97107@gmail.com>
2020-09-21 14:47:18 +0300

    createServer

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+
+
+const app = express()
+
+const PORT = process.env.PORT || 5000;
+
+
+app.get('/', (req, res) => {
+    res.send('API running')
+})
+
+app.listen(PORT, () => {
+    console.log('Server started op port ' + PORT)
+})
