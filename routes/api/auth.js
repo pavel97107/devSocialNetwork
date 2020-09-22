93a529755e14c329057f8a4bcc20537857830664
pavel <pavel97107@gmail.com>
2020-09-22 22:12:12 +0300

    did_a_tokenCheckForAuthenticity

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,11 +1,18 @@
 const { Router } = require("express");
+const auth = require("../../middleware/auth");
+const User = require('../../models/User')
 const router = Router();
 
 //@route GET api/users
 //@desc Test route
 //@access Public
-router.get("/", (req, res) => {
-  res.send("Auth route");
+router.get("/", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password')
+    res.json(user)
+  } catch(e) {
+    res.status(500).json(e)
+  }
 });
 
 module.exports = router;
