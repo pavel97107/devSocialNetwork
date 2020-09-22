7838fbcec6f7b40226edc6f19c5dbda0bfa70c65
pavel <pavel97107@gmail.com>
2020-09-22 23:16:02 +0300

    createRouteForUserProfile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,11 +1,24 @@
 const { Router } = require("express");
 const router = Router();
+const auth = require("../../middleware/auth");
+const Profile = require("../../models/Profile");
 
 //@route GET api/users
-//@desc Test route
+//@desc Get current user profile
 //@access Public
-router.get("/", (req, res) => {
-  res.send("Profile route");
+router.get("/me", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.user.id,
+    }).populate("user", ["name", "avatar"]);
+
+    if (!profile) {
+      return res.status(400).json({ msg: "There is no profile for this user" });
+    }
+  } catch (e) {
+    console.error(e.message);
+    res.status(500).send("Server Error");
+  }
 });
 
 module.exports = router;
