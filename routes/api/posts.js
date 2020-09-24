848741a3f0173aeb0ebc6f5e93302ea156dbbd0e
pavel <pavel97107@gmail.com>
2020-09-24 21:38:18 +0300

    add_remove_comment_routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -5,6 +5,7 @@ const auth = require("../../middleware/auth");
 const User = require("../../models/User");
 const Post = require("../../models/Post");
 const Profile = require("../../models/Profile");
+const { prependListener } = require("../../models/User");
 
 //@route POST api/posts
 //@desc Create post
@@ -20,18 +21,18 @@ router.post(
     try {
       const user = await User.findById(req.user.id).select("-password");
 
-      const newPost = Post({
+      const post = Post({
         text: req.body.text,
         name: user.name,
         avatar: user.avatar,
         user: req.user.id,
       });
-      await newPost.save();
+      await post.save();
 
-      res.status(201).json(newPost);
+      res.status(201).json(post);
     } catch (error) {
-      console.error(err.message);
-      req.status(500).send("Server Error");
+      console.error(error.message);
+      res.status(500).send("Server Error");
     }
   }
 );
@@ -107,9 +108,82 @@ router.put("/likes/:id", auth, async (req, res) => {
     );
 
     if (likePost.length > 0) {
-      post.likes = post.likes.filter((like) => like.user.toString() !== req.user.id);
+      post.likes = post.likes.filter(
+        (like) => like.user.toString() !== req.user.id
+      );
     } else {
       post.likes.unshift({ user: req.user.id });
+    }
+
+    await post.save();
+
+    res.status(200).json(post);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ errors: error });
+  }
+});
+
+//@route POST api/posts/comment/:id
+//@desc Add comment
+//@access Private
+router.post(
+  "/comment/:id",
+  [auth, [body("text", "Text is required").not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty())
+      return res.status(400).json({ errors: errors.array() });
+
+    try {
+      const user = await User.findById(req.user.id).select("-password");
+      const post = await Post.findById(req.params.id);
+
+      const newComment = {
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id,
+      };
+
+      post.comments.unshift(newComment);
+
+      await post.save();
+
+      res.status(201).json(post);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Server Error" + error.message);
+    }
+  }
+);
+
+//@route POST api/posts/comment/:id/:commentID
+//@desc Delete comment
+//@access Private
+
+router.delete("/comment/:id/:commentId", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    let comment = post.comments.find(
+      (comment) => comment.id === req.params.commentId
+    );
+
+    if (!comment)
+      return res.status(404).json({ msg: "Comment does not exist" });
+
+    let isMyPost = post.user.toString() === req.user.id;
+    let isMyComment = comment.user.toString() === req.user.id;
+
+    if (isMyPost || isMyComment) {
+      post.comments = post.comments.filter(
+        (comment) => comment.id !== req.params.commentId
+      );
+    } else {
+      return res
+        .status(400)
+        .json({ msg: "Нельзя удалять чужие коментарии не под своим постом" });
     }
 
     await post.save();
